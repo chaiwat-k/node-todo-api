811bde0a9354b3a69aa798620f6889c79742c641
chaiwat-k <chaiwat.kanchanarat@gmail.com>
2018-11-27 12:44:50 +0700

    add delete scripts

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+    if(err){
+        console.log('Unable to connect to MongoDB server');
+        return;
+    }
+    console.log('Connected MongoDB server');
+
+    // deleteMany
+    // db.collection('Todos').deleteMany({text:'Having lunch'})
+    // .then((result)=>{
+    //     console.log(result);
+    // })
+    // .catch((err)=>{
+    //     console.log('Unable to delete: ', err);
+    // });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne({text:'Having lunch'})
+    // .then((result)=>{
+    //     console.log(result);
+    // })
+    // .catch((err)=>{
+    //     console.log('Unable to delete: ', err);
+    // });    
+
+    // findOneAndDelete
+    db.collection('Todos').findOneAndDelete({completed:true})
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch((err)=>{
+        console.log('Unable to delete: ', err);
+    });
+
+    // db.close();
+});
