8ed1a2e0343989dd663b86a99cb0837b6bcf1287
chaiwat-k <chaiwat.kanchanarat@gmail.com>
2018-11-28 14:21:25 +0700

    Add GET /users/me and authentication

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var config = require('./config/config');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 app.use(bodyParser.json());
@@ -142,6 +143,11 @@ app.post('/users', (req, res)=>{
     .catch((err)=>{
         res.status(400).send(err);
     });   
+});
+
+// GET /users/me - to get token
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 const port = process.env.PORT || 3000;
