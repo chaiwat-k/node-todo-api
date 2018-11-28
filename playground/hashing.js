a8e93310dc48e575fb636113a2ae34c7fa0402b2
chaiwat-k <chaiwat.kanchanarat@gmail.com>
2018-11-28 13:29:53 +0700

    Add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,46 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id: 10
+};
+
+var secret = '123abc';
+
+var token = jwt.sign(data, secret);
+console.log('token: ',token);
+
+// Someone changes token or secret
+//token += 'xyz';
+//secret += 'xyz';
+
+var decoded = jwt.verify(token, secret);
+console.log('decoded: ',decoded);
+
+/* SIMULATION
+var message = 'I am user number 3';
+var hash = SHA256(message).toString();
+
+console.log('Message: ', message);
+console.log('Hash: ', hash);
+
+var data = {
+    id: 4
+};
+
+const salt = 'somesecret'; // salt is private on server
+var token = {
+    data,
+    hash: SHA256(JSON.stringify(data)+salt).toString()
+};
+
+// If someone changes the data and token's hash
+token.data.id = 5;
+token.hash = SHA256(JSON.stringify(data)).toString();
+
+var resultHash = SHA256(JSON.stringify(token.data)+salt).toString()
+if(resultHash === token.hash){
+    console.log('Data was not changed');
+}else{
+    console.log('Data was changed. Don\'t trust it.');
+}*/
--- server/server.js
+++ server/server.js
@@ -125,6 +125,25 @@ app.patch('/todos/:id', (req, res)=>{
 
 });
 
+// USER =========================
+
+// POST /users
+app.post('/users', (req, res)=>{
+    var body = _.pick(req.body,['email','password']);
+    var user = new User(body);
+    user.save()
+    .then(()=>{
+        return user.generateAuthToken();
+    })
+    .then((token)=>{
+        res.header('x-auth',token);
+        res.send(user);
+    })
+    .catch((err)=>{
+        res.status(400).send(err);
+    });   
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log('Started on port ', port);
