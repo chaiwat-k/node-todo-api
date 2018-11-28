60512691158f487c5c3e464de02dcba7586a95e9
chaiwat-k <chaiwat.kanchanarat@gmail.com>
2018-11-28 19:44:59 +0700

    add test for get /users/me and post /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,71 @@
+const {ObjectID} = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const {Todo} = require('./../../models/todo');
+const {User} = require('./../../models/user');
+
+const mockTodos = [
+    {
+        _id: new ObjectID(),
+        text:'1st test todo'
+    },
+    {
+        _id: new ObjectID(),
+        text:'2nd test todo',
+        completed:true,
+        completedAt: 222
+    }
+];
+
+const mockUserIDs = [
+    new ObjectID(),
+    new ObjectID()
+];
+const mockUsers = [
+    {
+        _id: mockUserIDs[0],
+        email: 'aj.styles@wwe.com',
+        password: 'ajrock',
+        tokens: [{
+            access: 'auth',
+            token: jwt.sign({_id: mockUserIDs[0],access:'auth'},'abc123').toString()
+        }]
+    },
+    {
+        _id: mockUserIDs[1],
+        email: 'ricochet@wwe.com',
+        password: 'prince_puma',
+        tokens: [{
+            access: 'auth',
+            token: jwt.sign({_id: mockUserIDs[1],access:'auth'},'abc123').toString()
+        }]
+    }
+];
+
+const populateTodos = (done) => {
+    Todo.remove({}) // Wipe out everything
+    .then(()=>{
+        return Todo.insertMany(mockTodos);
+    })
+    .then(()=>done())
+    .catch((err)=>done(err));
+};
+
+const populateUsers = (done) => {
+    User.remove({})
+    .then(()=>{
+        return Promise.all([
+            new User(mockUsers[0]).save(),
+            new User(mockUsers[1]).save(),
+        ]);
+    })
+    .then(()=>done())
+    .catch((err)=>done(err));
+};
+
+module.exports = {
+    mockTodos,
+    populateTodos,
+    mockUsers,
+    populateUsers
+};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,27 +4,11 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+const {mockTodos, populateTodos, mockUsers, populateUsers} = require('./seed/seed');
 
-const mockTodos = [
-    {
-        _id: new ObjectID(),
-        text:'1st test todo'
-    },
-    {
-        _id: new ObjectID(),
-        text:'2nd test todo',
-        completed:true,
-        completedAt: 222
-    }
-];
-
-beforeEach((done) => {
-    Todo.remove({}) // Wipe out everything
-    .then(()=>{
-        return Todo.insertMany(mockTodos);
-    })
-    .then(()=>done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', ()=>{
 
@@ -163,5 +147,73 @@ describe('PATCH /todos/:id', ()=>{
             expect(res.body.todo.completedAt).toNotExist();
         })
         .end(done);            
+    });
+});
+
+describe('GET /users/me', () => {
+    
+    it('should return user if authenticated', (done) => {
+        request(app)
+        .get('/users/me')
+        .set('x-auth', mockUsers[0].tokens[0].token)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body._id).toBe(mockUsers[0]._id.toHexString());
+            expect(res.body.email).toBe(mockUsers[0].email)            
+        })
+        .end(done);
+    });
+
+    it('should return a 401 if not authenticated.', (done) => {
+        request(app)
+        .get('/users/me')
+        .expect(401)
+        .expect((res)=>{
+            expect(res.body).toEqual({});
+        })
+        .end(done);
+    });
+});
+
+describe('POST /users', () => {
+    it('should create a user', (done) => {
+        var email = 'exaple@example.com';
+        var password = '123mnb';
+        request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toExist();
+            expect(res.body._id).toExist();
+            expect(res.body.email).toBe(email);
+        })
+        .end((err) => {
+            if(err) return done(err);
+            User.findOne({email})
+            .then((user)=>{
+                expect(user).toExist()
+                expect(user.password).toNotBe(password)
+                done();
+            });
+        });
+    });
+    it('should return validation errors if request is invalid', (done) => {
+        var email = 'invalid_email';
+        var password = '';
+        request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(400)
+        .end(done);
+    });
+    it('should not create user if email in use', (done)=>{
+        var email = mockUsers[0].email;
+        var password = 'hello_world';
+        request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(400)
+        .end(done);
     });
 });
